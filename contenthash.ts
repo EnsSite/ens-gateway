9422291013126c79c9add08c5cc7acf2e1d71399
Sem <931684+sembrestels@users.noreply.github.com>
2023-11-26 11:18:13 +0100

    Add error message when alchemy api is missing

--- contenthash.ts
+++ contenthash.ts
@@ -5,6 +5,11 @@ import { parseAbi } from 'viem/abi'
 import { createPublicClient, http } from 'viem'
 import { mainnet } from 'viem/chains'
 
+if (!process.env.ALCHEMY_API_KEY) {
+  console.error('\nError: Missing ALCHEMY_API_KEY environment variable.\n')
+  process.exit(1)
+}
+
 const transport = http(`https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`)
 
 const publicClient = createPublicClient({
